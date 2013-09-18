7f4133107e7937b641d3e338f742e4d6dbfc3ea6
Daniel Mermelstein <danmermel@gmail.com>
2013-09-18 21:36:34 +0000

    added the GPIO goodness

--- twitter.rb
+++ twitter.rb
@@ -1,6 +1,13 @@
 require 'net/http'
 require 'net/https'
 require 'json'
+require 'pi_piper'
+
+
+row = PiPiper::Pin.new( :pin => 17, :direction => :out)
+col = PiPiper::Pin.new( :pin => 18, :direction => :out)
+row.off  # start with everything off
+col.off 
 
 uri = URI.parse("https://api.twitter.com")
 http = Net::HTTP.new(uri.host, uri.port)  # new net http object
@@ -15,16 +22,27 @@ response = http.request(request)   # make the call
 # puts response.body  # this will show you the body
 res_parsed =JSON.parse(response.body)  # parse the response
 my_token =  res_parsed["access_token"] # grab the access token 
-
+puts "my token is #{my_token}"
 # now you can go and make the ACTUAL GET request
 
-request2 = Net::HTTP::Get.new("/1.1/statuses/user_timeline.json?screen_name=desktopdan&count=10", initheader = {"Authorization" => "Bearer #{my_token}"})
-response2 = http.request(request2)
-File.open('twitter.txt', 'w') do |f| 
-  f.write(response2.body)
+request2 = Net::HTTP::Get.new("/1.1/statuses/user_timeline.json?screen_name=desktopdan&count=1", initheader = {"Authorization" => "Bearer #{my_token}"})
+
+while true do
+   response2 = http.request(request2)
+   parsed_response = JSON.parse(response2.body)
+   parsed_response.each do |x|    # loop through data array
+      if x["text"] == "on" then
+         puts "turning on" 
+         row.on
+         col.on
+      elsif x["text"] == "off" then
+         puts "turning off"
+         row.off
+         col.off
+      else puts "did not understand text"
+      end
+   end
+   puts "waiting for a minute"
+   sleep 60 
 end
-parsed_response = JSON.parse(response2.body)
-parsed_response.each do |x|    # loop through data array
-   puts x["text"]
-end 
 
